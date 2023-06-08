9fffdf8446a9b03360535157efe304d5603a1ef9
rabia20kassi <rabiaakassi@gmail.com>
2023-06-09 00:12:39 +0100

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    hid = dir(hidden_4)
-    for i in range(len(hid)):
-        if (hid[i][:2] == "__"):
-            continue
-        print(hid[i])
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
