abffc399be5dd32a7304831303056bd22d51ad83
rabia20kassi <rabiaakassi@gmail.com>
2023-06-08 23:31:13 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1
+    if argc == 0 or argc == 1:
+        print("{} argument:".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+    for i in range(argc):
+        print("{}: {}".format(i + 1, argv[i + 1]))
